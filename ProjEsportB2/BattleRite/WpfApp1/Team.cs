97058d0b2ef3c9fc4c2529bee2e456ccfe09dc0d
MamaLeRattata <34157903+Faladus@users.noreply.github.com>
2018-04-05 15:52:02 +0200

    nts sylvain

---
+++ ProjEsportB2/BattleRite/WpfApp1/Team.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    class Team
+    {
+        public string Name { get; set; }
+        public int Id { get; set; }
+
+        public Team(string name)
+        {
+            Name = name;
+        }
+
+
+    }
+}
---
+++ ProjEsportB2/README.md
@@ -0,0 +1,8 @@
+# ProjEsportB2
+But : Développer une application client lourd pour permettre de gérer des compétitions d'Esport sur un jeu choisi.
+
+Sujet : Logiciel de gestion de compétition pour le jeu 'Battlerite'.
+
+Langage et technologies :</br>
+- C# (.NET) en application WPF</br>
+- un stockage en local avec Realm
